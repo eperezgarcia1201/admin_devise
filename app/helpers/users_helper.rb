69728bd930918b7178e6d4cf0f7f638c0ac71b40
ELMER <admin@aprode.org>
2016-12-20 02:50:08 -0600

    Added users index and profile editing

---
+++ app/helpers/users_helper.rb
@@ -0,0 +1,18 @@
+module UsersHelper
+
+	def avatar
+		image_tag(@user.avatar.url(:thumb), class: "show_user") 
+	end
+
+	def date_reg
+		time_ago_in_words(@user.created_at)
+	end
+
+	def name
+		@user.name
+	end
+
+	def correct_user?(user)
+		user == current_user
+	end
+end
